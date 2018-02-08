a2a65d4a8f78f09f7e43a6a23e7b92a8b1c94dcc
TheBicPen <mashkal2000@gmail.com>
2018-02-08 12:58:29 -0800

    added labels to show what is happening. Changed timer function

--- BC Green Games Vote Manipulator/Form1.Designer.cs
+++ BC Green Games Vote Manipulator/Form1.Designer.cs
@@ -35,6 +35,9 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // webBrowser1
@@ -62,7 +65,7 @@
             // timer1
             // 
             this.timer1.Enabled = true;
-            this.timer1.Interval = 300000;
+            this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // textBox1
@@ -89,11 +92,41 @@
             this.textBox2.Size = new System.Drawing.Size(249, 20);
             this.textBox2.TabIndex = 4;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Time Until Next Refresh:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(193, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Current Refresh Interval";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(225, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Current URL:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(466, 361);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
@@ -114,6 +147,9 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- BC Green Games Vote Manipulator/Form1.cs
+++ BC Green Games Vote Manipulator/Form1.cs
@@ -12,26 +12,38 @@ namespace BC_Green_Games_Vote_Manipulator
 {
     public partial class Form1 : Form
     {
+        int timeRemaining;
+        int interval;
         public Form1()
         {
             InitializeComponent();
             textBox1.Text = "https://www.bcgreengames.ca/vote/entityform/2199/1/vote/alternate/lXzxzSeOs71jxeGK27YQIIPSTI2kiL6ZQ2qDXKOQnu8/ajax";
             textBox2.Text = "300";
+            webBrowser1.Refresh();
+
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            webBrowser1.Refresh();
+            timeRemaining -= 1;
+            label1.Text = timeRemaining;
+            if(timeRemaining <= 0)
+            {
+                webBrowser1.Refresh();
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             webBrowser1.Navigate(textBox1.Text);
+            label3.Text += webBrowser1.Url.ToString();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            timer1.Interval = int.Parse(textBox2.Text) * 1000;
+            interval = int.Parse(textBox2.Text) * 1000;
+            timer1.Interval = interval;
+            label2.Text = timer1.Interval.ToString();
         }
     }
 }
